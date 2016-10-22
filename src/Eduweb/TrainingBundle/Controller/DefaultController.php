fcb1abc339ec18b50f6f68be806db148e04bf4ed
Michal Boszke <boszkem@gmail.com>
2016-10-22 15:30:04 +0200

    routing

--- src/Eduweb/TrainingBundle/Controller/DefaultController.php
+++ src/Eduweb/TrainingBundle/Controller/DefaultController.php
@@ -3,11 +3,18 @@
 namespace Eduweb\TrainingBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Response;
 
 class DefaultController extends Controller
 {
-    public function indexAction()
+    public function indexAction($name)
     {
-        return $this->render('EduwebTrainingBundle:Default:index.html.twig');
+        return $this->render('EduwebTrainingBundle:Default:index.html.twig', array('name' => $name));
+    }
+    
+    public function registerUserAction()
+    {
+        $responseMsg = "Rejestracja użytkownika";
+        return new Response($responseMsg);
     }
 }
