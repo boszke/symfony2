a4d8fc02a54cc5b06cff42f8b5c48d2a784a3e8a
Michal Boszke <boszkem@gmail.com>
2016-10-30 22:14:28 +0100

    kontrolery (odpowiedzi, przekierowania, przekazywanie, wyjątki, request, get)

--- src/Eduweb/TrainingBundle/Controller/DefaultController.php
+++ src/Eduweb/TrainingBundle/Controller/DefaultController.php
@@ -4,7 +4,12 @@ namespace Eduweb\TrainingBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Response;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 
+/**
+ * @Route("/eduweb")
+ */
 class DefaultController extends Controller
 {
     public function indexAction($name)
@@ -12,9 +17,69 @@ class DefaultController extends Controller
         return $this->render('EduwebTrainingBundle:Default:index.html.twig', array('name' => $name));
     }
     
-    public function registerUserAction()
+    public function registerUserAction($name, $age, $role)
     {
-        $responseMsg = "Rejestracja użytkownika";
+        $responseMsg = sprintf("Rejestracja użytkownika o nazwie %s (wiek: %d), rola w systemie %s", $name, $age, $role);
         return new Response($responseMsg);
     }
+    
+    public function simple1Action() {
+        return new Response("Simple1");
+    }
+    
+    public function simple2Action() {
+        return new Response("Simple2");
+    }
+    /**
+     * 
+     * @Route(
+     *      "/register-tester/{name}-{age}-{role}",
+     *      name="eduweb_trainig_registerTester",
+     *      defaults={"role"="units"},
+     *      requirements={"age"="\d+", "role"="units|functional"}
+     * )
+     * 
+     * @Method({"GET"})
+     */
+    public function registerTesterAction($name, $age, $role) {
+        $responseMsg = sprintf("Rejestracja testera o nazwie %s (wiek: %d), rola w systemie %s", $name, $age, $role);
+        return new Response($responseMsg);
+    }
+    /**
+     * @Route("/anno")
+     */
+    public function anno2Action() {
+        return new Response("anno2");
+    }
+    /**
+     * 
+     * @Route("/anno")
+     */
+    public function anno1Action() {
+        return new Response("anno1");
+    }
+    
+    /**
+     * 
+     * @Route("/generate-url")
+     */
+    public function generateUrlAction() {
+        $response = $this->generateUrl('eduweb_training_registerUser', array(
+            'name'=> 'Marcin',
+            'age'=> 52,
+            'country' => 'Poland'
+        ), TRUE );
+        return new Response($response);
+    }
+    
+    /**
+     * @Route(
+     *      "/debugging",
+     *      name="eduweb_training_debugging"
+     * )
+     */
+    public function debuggingAction() {
+        return new Response('<html><head><title>Debugging</title></head><body>debugging</body></html>');
+    }
+    
 }
